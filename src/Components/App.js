ef6ad8c75efd3048316f375cf6b5572d9032b1b0
정연수 <jys1015@hanyang.co.kr>
2020-01-10 15:05:07 +0900

    Global Css added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Router from "Components/Router";
-
+import GlobalStyle from "Components/GlobalStyles";
 class App extends Component{
   render() {
     return (
       <>
         <Router />
+        <GlobalStyle/>
       </>
     );
   };
