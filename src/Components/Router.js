543b67992f46af3f364e54443280daa87314d3c3
정연수 <jys1015@hanyang.co.kr>
2020-01-14 15:33:04 +0900

    Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
-import Search from "Routes/Search";
 import Header from "Components/Header";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
     <Router>
@@ -14,6 +15,8 @@ export default () => (
                 {/* 여러개가 있을경우 exact 써서 구분해준다. */}
                 <Route path="/tv" exact component={TV}/>
                 <Route path="/search" component={Search}/>
+                <Route path="/movie/:id" component={Detail}/>
+                <Route path="/show/:id" component={Detail}/>
                 <Redirect from="*" to="/"/>
             </Switch>
         </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { movies, tv } from "api";
 
 export default class extends React.Component{
     state = {
@@ -9,8 +10,38 @@ export default class extends React.Component{
         loading: false,
         error: null
     };
+
+    
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if (searchTerm !== ""){
+            this.searchByTerm();
+        }
+    }
+
+    searchByTerm = async() => {
+        let error, movieResults, tvResults, loading;
+        const { searchTerm } = this.state;
+        try{
+            ({data: { results: movieResults}} = await movies.searchMovies(searchTerm));
+            ({data: { results: tvResults}} = await tv.searchTV(searchTerm));
+            loading = true
+        }catch{
+            error = "Can't find Result";
+        }finally{
+            this.setState({
+                loading,
+                error,
+                movieResults,
+                tvResults
+
+            });
+        }
+    }
+
     render() {
         const {movieResults, tvResults, searchTerm, loading, error} = this.state;
+        console.log(this.state);
         return (
             <SearchPresenter
                 movieResults={movieResults}
@@ -18,6 +49,7 @@ export default class extends React.Component{
                 searchTerm={searchTerm}
                 loading={loading}
                 error={error}
+                handleSubmit={this.handleSubmit}
             />
         )
     }
