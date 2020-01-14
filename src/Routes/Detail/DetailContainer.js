faf7bc46186340052c29946a2f03b977557059b2
정연수 <jys1015@hanyang.co.kr>
2020-01-14 17:14:58 +0900

    DetailContainer

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,14 +1,58 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter"
+import { movies, tv } from "api";
 
 export default class extends React.Component{
-    state = {
-        result: null,
-        error: null,
-        loading: true
-    };
+    constructor(props){
+        super(props);
+        const { location: { pathname}} = props;
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        };
+    }
+    
+
+    async componentDidMount(){
+        let error, result
+        // 라우터는 url에 대한 정보를 props로 넘겨준다. match: {params:{지정한 이름}}
+        const { 
+            match: {
+                params: {id}
+            },
+            history: { push }
+        } = this.props;
+
+        const {isMovie } = this.state;
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)){
+            return push("/");
+        }
+        
+        try{
+            if (isMovie){
+                ({data: result} = await movies.getMovie(parsedId));
+            }else{
+                ({data: result} = await tv.getShow(parsedId));
+            }
+            console.log(result);
+        }catch{
+            error = "Can't find moview"
+        }finally{
+            this.setState({
+                loading:false,
+                result,
+                error
+            })
+        }
+        
+    }
+
     render() {
         const {result, error, loading} = this.state;
+        
         return (
             <DetailPresenter
                 result={result}
