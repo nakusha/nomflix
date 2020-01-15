c601c9254f9a996cb531d68d9ef44b5732a31e26
정연수 <jys1015@hanyang.co.kr>
2020-01-15 13:49:13 +0900

    prepare Presenters(proptypes and export)

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,13 @@
-export default () => "Detail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const DetailPresenter = ({loading, result, error}) => null
+
+DetailPresenter.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    result: PropTypes.array.isRequired,
+    error: PropTypes.string
+};
+
+export default DetailPresenter;
